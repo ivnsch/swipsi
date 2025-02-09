b3fe4227b41e66fdddcd85f2655c353299114fc6
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-09 17:05:04 +0100

    prevent warning with string array with swiftdata
    didn't *seem* to be causing issues, but anyway

--- app/likes/LikedBike.swift
+++ app/likes/LikedBike.swift
@@ -7,13 +7,22 @@ final class LikedBike: Identifiable, Hashable {
     var name: String = ""
     var brand: String = ""
     var price: String = ""
-    var pictures: [String] = []
+    var picturesString: String = ""
     var likedDate: Date = Date()
     var vendorLink: String = ""
     var type: String = ""
     var electric: Bool = false
     var descr: String = ""
     var order: Int = 0
+    
+    var pictures: [String] {
+           get {
+               picturesString.isEmpty ? [] : picturesString.components(separatedBy: ",")
+           }
+           set {
+               picturesString = newValue.joined(separator: ",")
+           }
+       }
     
     init(id: String, name: String, brand: String, price: String, pictures: [String], likedDate: Date, vendorLink: String, type: String, electric: Bool, descr: String, order: Int) {
         self.id = id
