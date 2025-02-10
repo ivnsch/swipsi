db6cc509c84e19ba25ec59a74f8ee53ed7550ebc
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-10 05:46:31 +0100

    move to custom view

--- app/likes/LikedBikesView.swift
+++ app/likes/LikedBikesView.swift
@@ -14,40 +14,7 @@ struct LikedBikesView: View {
                         NavigationLink {
                             LikedBikeDetailsView(bike: bike)
                         } label: {
-                            HStack {
-                                // TODO handle no pic
-                                AsyncImage(url: URL(string: bike.pictures[0])) { phase in
-                                    if let image = phase.image {
-                                        image
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: 100, height: 100)
-                                            .clipped()
-                                    } else if phase.error != nil {
-                                        Color.red
-                                    } else {
-                                        ProgressView()
-                                    }
-                                }
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-
-                                VStack(alignment: .leading) {
-                                    Text(bike.name)
-                                    Text(bike.brand)
-                                    Text(bike.price)
-                                    HStack {
-                                        Text(bike.type)
-                                            .font(.system(size: 10))
-                                            .padding(4)
-                                            .borderedBgLight(color: .black)
-                                        Text(bike.electric ? "Electric" : "Non-Electric")
-                                            .font(.system(size: 10))
-                                            .padding(4)
-                                            .borderedBgLight(color: .black)
-                                    }
-                                }
-                                
-                            }
+                            LikeView(bike: bike)
                         }
                     }
                     .onMove(perform: { indexSet, dest in
@@ -89,3 +56,43 @@ struct LikedBikesView: View {
     }
 }
 
+private struct LikeView: View {
+    var bike: LikedBike
+
+    var body: some View {
+        HStack {
+            // TODO handle no pic
+            AsyncImage(url: URL(string: bike.pictures[0])) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                } else if phase.error != nil {
+                    Color.red
+                } else {
+                    ProgressView()
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+
+            VStack(alignment: .leading) {
+                Text(bike.name)
+                Text(bike.brand)
+                Text(bike.price)
+                HStack {
+                    Text(bike.type)
+                        .font(.system(size: 10))
+                        .padding(4)
+                        .borderedBgLight(color: .black)
+                    Text(bike.electric ? "Electric" : "Non-Electric")
+                        .font(.system(size: 10))
+                        .padding(4)
+                        .borderedBgLight(color: .black)
+                }
+            }
+            
+        }
+    }
+}
