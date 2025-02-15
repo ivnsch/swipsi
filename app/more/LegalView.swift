27540ad187a19ca61704ab4d0694a0da300b225d
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-15 18:23:25 +0100

    add terms

--- app/more/LegalView.swift
+++ app/more/LegalView.swift
@@ -12,7 +12,45 @@ struct LegalView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                Text(
+                     """
+Terms and Conditions
+                     
+Last Updated: 15.02.2025
+
+Welcome to swapsi (the "App"). By using this App, you agree to the following terms and conditions. If you do not agree, please do not use the App.
+
+1. Affiliate Disclosure
+
+This App contains links to third-party e-commerce platforms, including Amazon, where you can purchase products. These links include an affiliate ID, meaning we may earn a commission on purchases made through them at no additional cost to you.
+
+2. Third-Party Links
+
+The App provides links to external websites (e.g., Amazon). We do not own, operate, or control these third-party platforms and are not responsible for their content, policies, or practices. Your interactions with these third parties are governed by their respective terms and conditions.
+
+3. No Guarantee of Availability
+
+Product listings, prices, and availability may change without notice. Prices displayed in the App are updated daily on a best effort basis but may not always reflect the latest pricing on third-party platforms. We do not guarantee that any listed product will be available for purchase or that prices displayed in the App are current.
+
+4. Limitation of Liability
+
+We provide this App "as is" without warranties of any kind. We are not responsible for any issues, including but not limited to, inaccurate product information, transaction failures, or any damages resulting from using the App or third-party links.
+
+5. User Responsibility
+
+By using the App, you agree to review all product details, terms, and conditions on the third-party platform before making a purchase. We are not responsible for disputes between you and any external seller.
+
+6. Changes to These Terms
+
+We reserve the right to update these terms at any time. Continued use of the App after modifications constitutes your acceptance of the revised terms.
+
+7. Contact Information
+
+If you have any questions, you can contact us at https://discord.gg/Pwx7Sh8Suf.
+
+By using this App, you acknowledge that you have read and agreed to these Terms and Conditions.
+
+""")
                     .padding(20)
                     .foregroundColor(Color.black)
 
